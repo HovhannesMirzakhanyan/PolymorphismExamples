b0ebe271649e7fbfa7c76ce2980fa8df365119fc
HovhannesMirzakhanyan <h.r.mirzakhanyan@gmail.com>
2021-11-13 17:19:51 +0400

    Add project files.

---
+++ PolymorphismExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticPolymorphismExample example = new StaticPolymorphismExample();
+            int add1 = example.Add(25,36,98);
+            int add2 = example.Add(15, 20);
+        }
+    }
+}
+
---
+++ PolymorphismExamples/StaticPolymorphismExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExamples
+{
+    class StaticPolymorphismExample
+    {
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
